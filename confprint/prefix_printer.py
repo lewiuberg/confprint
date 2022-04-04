7bf089f59a6f28f08c1d52393ea8caad89d193ee
Lewi Uberg <lewiuberg@icloud.com>
2022-04-04 20:49:33 +0200

    Rename end and add args and kwargs

--- confprint/prefix_printer.py
+++ confprint/prefix_printer.py
@@ -21,7 +21,7 @@ def prefix_printer(
     upper: bool = True,
     frame_left: str = "[",
     frame_right: str = "]",
-    end=":",
+    prefix_end=":",
     counter_start: int = -1,
     global_counter: bool = False,
     text_color: str = "",
@@ -31,13 +31,16 @@ def prefix_printer(
     prefix_style: str = "",
     prefix_bg_color: str = "",
     format_frames: bool = True,
+    *args,
+    **kwargs,
 ) -> Callable[[str], None]:
     """
     Prefix printer is function factory for prefixing text.
 
     Args:
         prefix (Any): The prefix to use.
-        whitespace (int, optional): The number of whitespaces to use. Defaults to 1.
+        whitespace (int, optional): The number of whitespaces to use.
+        Defaults to 1.
         stderr (bool, optional):
             If True, the printer will print to sys.stderr instead of sys.stdout
             Defaults to False.
@@ -46,16 +49,18 @@ def prefix_printer(
         upper (bool, optional): If True, the prefix will be printed in upper
         frame_left (str, optional): The left frame. Defaults to "[".
         frame_right (str, optional): The right frame. Defaults to "]".
-        end (str, optional): The end of the prefix. Defaults to ":".
+        prefix_end (str, optional): The end of the prefix. Defaults to ":".
         counter_start (int, optional): The counter start value. Defaults to -1.
         global_counter (bool, optional): If True, the counter will be global.
             Defaults to False.
         text_color (str, optional): The text color. Defaults to "".
         text_style (str, optional): The text style. Defaults to "".
-        text_bg_color (str, optional): The text background color. Defaults to "".
+        text_bg_color (str, optional): The text background color.
+        Defaults to "".
         prefix_color (str, optional): The prefix color. Defaults to "".
         prefix_style (str, optional): The prefix style. Defaults to "".
-        prefix_bg_color (str, optional): The prefix background color. Defaults to "".
+        prefix_bg_color (str, optional): The prefix background color.
+        Defaults to "".
         format_frames (bool, optional): If True, the frames will be formatted.
             Defaults to True.
         format_frames (bool, optional): If True, the frames will be formatted.
@@ -81,7 +86,9 @@ def prefix_printer(
         global_count["n"] = global_count["l"] = counter_start
         count = global_count
 
-    def prefixed_printer(text: str = "", prefix: str = prefix) -> None:
+    def prefixed_printer(
+        text: str = "", prefix: str = prefix, *args, **kwargs
+    ) -> None:
         """
         Print text prefixed with the prefix.
 
@@ -128,14 +135,20 @@ def prefix_printer(
             prefix = f"{prefix_fmt_start}{prefix}{prefix_fmt_end}"
 
         if counter_start > -1 and not global_counter:
-            prefix = f"{frame_left}{prefix}{end}{count['n']}{frame_right}"
+            prefix = (
+                f"{frame_left}{prefix}{prefix_end}{count['n']}{frame_right}"
+            )
             count["n"] += 1
         elif counter_start > -1 and global_counter:
-            prefix = f"{frame_left}{prefix}{end}{count['n']}{frame_right}"
+            prefix = (
+                f"{frame_left}{prefix}{prefix_end}{count['n']}{frame_right}"
+            )
             count["n"] += 1
             global_count["l"] += 1
         elif counter_start == -1 and global_counter:
-            prefix = f"{frame_left}{prefix}{end}{count['n']}{frame_right}"
+            prefix = (
+                f"{frame_left}{prefix}{prefix_end}{count['n']}{frame_right}"
+            )
             count["n"] += 1
         else:
             prefix = f"{frame_left}{prefix}{frame_right}"
@@ -161,19 +174,19 @@ def prefix_printer(
             first_line_len = len(lines[0])
             lines[
                 0
-            ] = f"{prefix}{end} {' ' * whitespace}{text_fmt_start}{lines[0]}{text_fmt_end}"
+            ] = f"{prefix}{prefix_end} {' ' * whitespace}{text_fmt_start}{lines[0]}{text_fmt_end}"
             indent_len = (
                 len(lines[0])
                 - first_line_len
                 - len(text_fmt_start)
                 - len(text_fmt_end)
             )
-            print_func(lines[0])  # type: ignore
+            print_func(lines[0], *args, **kwargs)  # type: ignore # mypy bug
             [print_func((" " * indent_len) + f"{text_fmt_start}{line}{text_fmt_end}") for line in lines[1:]]  # type: ignore # mypy bug # noqa: E501
 
         elif not text:
-            print_func(f"{prefix}")  # type: ignore # mypy bug
+            print_func(f"{prefix}", *args, **kwargs)  # type: ignore # mypy bug
         else:
-            print_func(f"{prefix}{end} {' ' * whitespace}{text_fmt_start}{text}{text_fmt_end}")  # type: ignore # mypy bug
+            print_func((f"{prefix}{prefix_end} {' ' * whitespace}{text_fmt_start}{text}{text_fmt_end}"), *args, **kwargs)  # type: ignore # mypy bug # noqa: E501
 
     return prefixed_printer
